8d29617bf0f8e71ca729b24c502c419ecffa007e
Sami Hostikka <sami@01.fi>
2011-05-16 00:04:55 +0300

    fixed pagetab issues

---
+++ PersonalWiki/PersonalWiki/Model/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonalWiki.Model
+{
+    static class Settings
+    {
+//        public int SaveInterval { get; set; }
+        static public int SaveInterval = 1200;
+
+    }
+}
